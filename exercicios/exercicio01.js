c516bd1958612eb37c7781311b4c8db324db2aa3
wabemusic <jvwatanabe@gmail.com>
2025-05-06 19:50:59 -0300

    fix: melhorias no código

--- exercicios/exercicio01.js
+++ exercicios/exercicio01.js
@@ -1,6 +1,8 @@
 // 1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
 // utilizando uma estrutura de controle if.
 
+const prompt = require("prompt-sync")();
+
 const ehPar = (numInteiro) => {
   if (numInteiro % 2 === 0) {
     return `O numero ${numInteiro} é par!`;
@@ -9,4 +11,17 @@ const ehPar = (numInteiro) => {
   }
 };
 
-console.log(ehPar(2498721632));
+let numero;
+
+while (true) {
+  const entrada = prompt("Digite um número inteiero: ");
+  numero = parseInt(entrada, 10);
+
+  if (!isNaN(numero)) {
+    break;
+  }
+
+  console.log("Entrada inválida. Digite um número inteiro.");
+}
+
+console.log(ehPar(numero));
