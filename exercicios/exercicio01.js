f00fcc542b77b822b4d952a6194a55871a5b18c8
wabemusic <jvwatanabe@gmail.com>
2025-05-06 19:27:55 -0300

    add: exercício 01

--- exercicios/exercicio01.js
+++ exercicios/exercicio01.js
@@ -1,3 +1,12 @@
 // 1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar
 // utilizando uma estrutura de controle if.
 
+const ehPar = (numInteiro) => {
+  if (numInteiro % 2 === 0) {
+    return `O numero ${numInteiro} é par!`;
+  } else {
+    return `O numero ${numInteiro} é impar!`;
+  }
+};
+
+console.log(ehPar(2498721632));
