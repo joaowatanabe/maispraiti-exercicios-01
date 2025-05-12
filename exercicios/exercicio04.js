3098e96838413bf79ecc6e466dcad112c6c1a81b
wabemusic <jvwatanabe@gmail.com>
2025-05-11 21:26:25 -0300

    add: exercício 04

--- exercicios/exercicio04.js
+++ exercicios/exercicio04.js
@@ -1,1 +1,26 @@
 // 4. Crie um menu interativo no console que oferece ao usuário a escolha de três opções. Utilize switch-case para implementar a lógica de cada opção selecionada.
+
+const prompt = require("prompt-sync")();
+
+while (true) {
+  console.log("--- MENU ---");
+  console.log("1 - Dizer Olá Mundo");
+  console.log("2 - Mostrar data");
+  console.log("3 - Sair");
+
+  const opcao = prompt("Escolha uma opção (1, 2 ou 3): ");
+
+  switch (opcao) {
+    case "1":
+      console.log("Olá Mundo");
+      break;
+    case "2":
+      console.log("Data atual:", new Date().toLocaleString());
+      break;
+    case "3":
+      console.log("Encerrando o programa...");
+      return;
+    default:
+      console.log("Opção inválida. Tente novamente.");
+  }
+}
