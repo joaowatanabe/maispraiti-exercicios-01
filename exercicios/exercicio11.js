df6bc93203ed77f70fdaf5d3db51aadbe5d31700
wabemusic <jvwatanabe@gmail.com>
2025-05-13 20:44:23 -0300

    add: exercício 11

--- exercicios/exercicio11.js
+++ exercicios/exercicio11.js
@@ -1,2 +1,25 @@
 // 11. Escreva um programa que solicita ao usuário 5 números e calcula a soma total
 // utilizando um loop for.
+
+const prompt = require("prompt-sync")();
+
+const somaTotal = () => {
+  let soma = 0;
+
+  for (let i = 1; i <= 5; i++) {
+    let numero;
+    do {
+      numero = parseFloat(prompt(`Digite o ${i}º numero: `));
+
+      if (isNaN(numero)) {
+        console.log("Valor inválido, insira um valor válido.");
+      }
+    } while (isNaN(numero));
+
+    soma += numero;
+  }
+
+  console.log("A soma total é:", soma);
+};
+
+somaTotal();
