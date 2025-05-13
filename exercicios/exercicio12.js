3fab70989732695d20a038ef81528afe7ef158d0
wabemusic <jvwatanabe@gmail.com>
2025-05-13 20:54:59 -0300

    add: exercício 12

--- exercicios/exercicio12.js
+++ exercicios/exercicio12.js
@@ -1,2 +1,20 @@
 // 12. Crie um programa que exibe a tabuada de um número fornecido pelo usuário (de 1 a
 //   10) utilizando um loop for.
+
+const prompt = require("prompt-sync")();
+
+const tabuada = (n) => {
+  let numero = parseFloat(prompt(`Digite o numero da tabuada: `));
+
+  if (isNaN(numero)) {
+    console.log("Valor inválido, insira um valor válido.");
+    return;
+  }
+  console.log(`Tabuada do ${numero}:`);
+
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${numero} x ${i} = ${numero * i}`);
+  }
+};
+
+tabuada();
