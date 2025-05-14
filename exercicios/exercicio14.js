d91e50facfc28583d3a825c6db0da3c6766d0fd3
wabemusic <jvwatanabe@gmail.com>
2025-05-14 20:16:42 -0300

    add: exercício 14

--- exercicios/exercicio14.js
+++ exercicios/exercicio14.js
@@ -1,2 +1,27 @@
 // 14. Crie um programa que calcula o fatorial de um número fornecido pelo usuário
 // utilizando um loop for ou while.
+
+const prompt = require("prompt-sync")();
+
+const fatorial = (numero) => {
+  let resultado = 1;
+  let i = numero;
+
+  while (i > 1) {
+    resultado *= i;
+    i--;
+  }
+
+  return resultado;
+};
+
+const entrada = parseInt(
+  prompt("Digite um numero para descobrir o fatorial: ")
+);
+
+if (entrada < 0) {
+  console.log("Fatorial não existe em numeros negativos");
+} else {
+  let resultado = fatorial(entrada);
+  console.log(`O fatorial de ${entrada} é: ${resultado}`);
+}
